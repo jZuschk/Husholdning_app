848116ad5ab31f05ebc305cf37f3716687e2c7c8
Noire927 <Morten927@hotmail.com>
2021-11-25 18:33:50 +0100

    changed some html in home page

--- Problem_identifyer_dropdrown.js
+++ Problem_identifyer_dropdrown.js
@@ -1,14 +1,13 @@
 /*Dette er en TEST aka jeg leger - Skyum */
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+/* When the user clicks on the button, toggle between hiding and showing the dropdown menu */
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("dropdownmenu").classList.toggle("show");
   }
   
-  // Close the dropdown menu if the user clicks outside of it
+  /* Close the dropdown menu if the user clicks outside of it */
   window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
+    if (!event.target.matches('.dropdown_button')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
