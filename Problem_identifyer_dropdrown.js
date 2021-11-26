0669dd232b89e295379ee321dc6fe8fb05f6fc1e
Noire927 <Morten927@hotmail.com>
2021-11-26 10:39:45 +0100

    addes js doesn't work 100%

--- Problem_identifyer_dropdrown.js
+++ Problem_identifyer_dropdrown.js
@@ -1,4 +1,4 @@
-/* OPS - Der skal laves en JS fil mere/tilføjes classes/sættes ind i hver sin folder (Pletter/Rengoering), til Rengoerings siden */
+/* OPS - Der skal laves en JS fil mere/tilføjes classes/sættes ind i hver sin folder (Pletter/Rengoering) til Rengoerings siden */
 
 /* Opens the menu when the user clicks on it */
 function myFunction() {
@@ -18,3 +18,23 @@ function myFunction() {
       }
     }
   }
+
+/*--------------------------------------*/
+
+function myFunction() {
+  document.getElementById("dropdownmenu2").classList.toggle("show");
+}
+
+/* Closes when you click outside */
+window.onclick = function(event) {
+  if (!event.target.matches('.dropdown_button2')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content2");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
